9ccaea8282de058f46b4ef9dd0666237d314758c
kevin <kevinrahul.22ec@kct.ac.in>
2025-03-05 18:53:29 +0530

    Footer Resources Aligned

--- src/components/footer-component/footer.tsx
+++ src/components/footer-component/footer.tsx
@@ -10,8 +10,8 @@ const Footer = () => {
     <footer className="bg-pink-50 dark:bg-neutral-900 md:mt-0">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
         <div className="md:flex md:justify-between">
-          <div className="mb-6 md:mb-0">
-            <Link href={"/"} className="flex items-center">
+          <div className="mb-3 md:mb-0">
+            <Link href={"/"} className="flex items-center justify-start">
               <Image src={Logo} width={75} height={75} alt="Logo" />
               <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
                 Kv Mahal
@@ -19,9 +19,9 @@ const Footer = () => {
             </Link>
           </div>
           {/* <div className="grid grid-cols-2 gap-8 bg-slate-400 sm:gap-6 sm:grid-cols-3"> */}
-          <div className=" mr-24  ">
+          <div className=" mr-24 ml-4">
             <div>
-              <h2 className="mb-6 text-sm font-semibold text-center text-gray-900 uppercase dark:text-white">
+              <h2 className="md:mb-6 mb-3 text-sm font-semibold md:text-center  text-gray-900 uppercase dark:text-white">
                 Resources
               </h2>
               <ul className="text-gray-500 grid grid-cols-2 gap-3 dark:text-gray-400 font-medium">
@@ -43,28 +43,7 @@ const Footer = () => {
                     <Link className="hover:underline hover:dark:text-pink-300" href={""}>Map</Link></li>
               </ul>
             </div>
-            {/* <div>
-                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Follow us</h2>
-                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                      <li className="mb-4">
-                          <a href="https://github.com/themesberg/flowbite" className="hover:underline ">Github</a>
-                      </li>
-                      <li>
-                          <a href="https://discord.gg/4eeurUVvTy" className="hover:underline">Discord</a>
-                      </li>
-                  </ul>
-              </div>
-              <div>
-                  <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">Legal</h2>
-                  <ul className="text-gray-500 dark:text-gray-400 font-medium">
-                      <li className="mb-4">
-                          <a href="#" className="hover:underline">Privacy Policy</a>
-                      </li>
-                      <li>
-                          <a href="#" className="hover:underline">Terms &amp; Conditions</a>
-                      </li>
-                  </ul>
-              </div> */}
+            
           </div>
         </div>
         <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
@@ -72,7 +51,7 @@ const Footer = () => {
           <span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
             © 2025{" "}
             <a href="https://flowbite.com/" className="hover:underline">
-              Vk Mahal™
+              Kv Mahal™
             </a>
             . All Rights Reserved.
           </span>
