b68cce1acd6418ae6824493f59b386266aa5b500
kevin <kevinrahul.22ec@kct.ac.in>
2025-03-19 12:21:23 +0530

    Testimonial Are connected with the db

--- src/components/ui/testimonialcard.tsx
+++ src/components/ui/testimonialcard.tsx
@@ -1,74 +1,37 @@
+"use client";
 import { cn } from "@/lib/utils";
 import { Marquee } from "@/components/magicui/marquee";
 import { ReactNode } from "react";
 import { AuroraText } from "../magicui/aurora-text";
+import useCustomer from "@/app/admin/Actions/useCustomer";
 
 
-const reviews =[
-    {
-        id:1,
-      name:"Kevin",
-      username:"@kevin",
-      img: "https://avatar.vercel.sh/jane",
-      body:"lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-    },
-    {
-        id:2,
-      name:"Rahul",
-      // username:"@kevin",
-      img: "https://avatar.vercel.sh/jane",
-      body:"lorem ipsum dolor sit amet,lorem ipsum dolor sit amet, consectetur adipiscing elit,lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-    },
-    
-    {
-        id:3,
-      name:"Mervin",
-      img: "https://avatar.vercel.sh/jane",
-      username:"@Mervin",
-      body:"lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-    },
-    {
-        id:4,
-      name:"kumar",
-      img: "https://avatar.vercel.sh/jane",
-      username:"@Kumara",
-      body:"lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-    },
-    {
-        id:5,
-      name:"Jhon",
-      img: "https://avatar.vercel.sh/jane",
-      username:"Kevinkrahul@gmail.com",
-      body:"lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-    },
-    {
-        id:6,
-      name:"don",
-      // username:"@kevin",
-      img: "https://avatar.vercel.sh/jane",
-      body:"lorem ipsum dolor sit amet,lorem ipsum dolor sit amet, consectetur adipiscing elit,lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam."
-    },
-  ]
-  export const firstRow = reviews.slice(0, reviews.length / 2);
-  export const secondRow = reviews.slice(reviews.length / 2);
-  
+export function customerReview(){
+const {customer}=useCustomer();
+const firstRow = customer.slice(0, customer.length / 2);
+const secondRow = customer.slice(customer.length / 2);
 
+return{
+  firstRow,
+  secondRow,
+  customer
+}
 
-
+}
 
 
 const ReviewCard = ({
     // id,
     // img,
     name,
-    username,
-    body,
+    email,
+    comments,
   }: {
     // id:number;
     // img?: string;
     name: string;
-    username?: string;
-    body: string;
+    email?: string;
+    comments: string;
   }) => {
     return (
       <figure
@@ -86,16 +49,18 @@ const ReviewCard = ({
             <figcaption className="text-sm font-medium text-left dark:text-white">
               {name}
             </figcaption>
-            <p className="text-xs font-medium dark:text-white/40">{username}</p>
+            <p className="text-xs font-medium dark:text-white/40">{email}</p>
           </div>
         </div>
-        <blockquote className="mt-2 text-sm">{body}</blockquote>
+        <blockquote className="mt-2 text-sm">{comments}</blockquote>
       </figure>
     );
   };
 
 
 const Testimonial=({children}:{children:ReactNode})=>{
+
+  const {firstRow,secondRow}=customerReview();
 
     return(
         <section className="flex flex-col  items-center w-full pb-10">
@@ -106,12 +71,12 @@ const Testimonial=({children}:{children:ReactNode})=>{
           Discover the perfect venue for your special day. We
         </p>
         <div className="relative flex w-full flex-col items-center justify-center overflow-hidden">
-          <Marquee pauseOnHover className="[--duration:40s]">
+          <Marquee pauseOnHover className="[--duration:25s]">
             {firstRow.map((review) => (
               <ReviewCard key={review.id} {...review} />
             ))}
           </Marquee>
-          <Marquee reverse pauseOnHover className="[--duration:40s]">
+          <Marquee reverse pauseOnHover className="[--duration:25s]">
             {secondRow.map((review) => (
               <ReviewCard key={review.id} {...review} />
             ))}
