7c4f4f2630f48eec0421ee133b5b94590a6fa331
kevin <kevinrahul.22ec@kct.ac.in>
2025-03-19 11:26:28 +0530

    Admin page customized successfully

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -1,5 +1,4 @@
 "use client";
-import { useState } from "react";
 import DynamicForm from "@/components/form/DynamicForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
@@ -9,6 +8,13 @@ import useCustomer from "./Actions/useCustomer";
 import useImage from "./Actions/useImage";
 import useDateEvent from "./Actions/useDateEvent";
 import { useEditing } from "./Actions/editingHook";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "@/components/ui/carousel";
 
 export default function Admin() {
   const {
@@ -45,191 +51,253 @@ export default function Admin() {
     <>
       {/* FAQ */}
       <div>
-        <h1 className="text-xl font-bold mb-4">
-          {editingItem?.type === "faq" ? "Edit FAQ" : "Create FAQ"}
-        </h1>
-        <DynamicForm
-          key={editingItem?.id || "new"}
-          schema={faqSchema}
-          defaultValues={
-            editingItem?.type === "faq"
-              ? {
-                  question: editingItem.data.questions,
-                  answer: editingItem.data.answers,
-                }
-              : { question: "", answer: "" }
-          }
-          fields={[
-            {
-              name: "question",
-              label: "Question",
-              placeholder: "Enter your question",
-            },
-            {
-              name: "answer",
-              label: "Answer",
-              placeholder: "Enter your answer",
-            },
-          ]}
-          submitHandler={handleFaqSubmit}
-          loading={faqloading}
-          editing={editingItem?.type === "faq"}
-        />
-        <div>
+        <div className="flex flex-col gap-4 bg-pink-50 items-center p-4"> 
+          <h1 className="text-xl font-bold mb-4">
+            {editingItem?.type === "faq" ? "Edit FAQ" : "Create FAQ"}
+          </h1>
+          <DynamicForm
+            key={editingItem?.id || "new"}
+            schema={faqSchema}
+            defaultValues={
+              editingItem?.type === "faq"
+                ? {
+                    question: editingItem.data.questions,
+                    answer: editingItem.data.answers,
+                  }
+                : { question: "", answer: "" }
+            }
+            fields={[
+              {
+                name: "question",
+                label: "Question",
+                placeholder: "Enter your question",
+              },
+              {
+                name: "answer",
+                label: "Answer",
+                placeholder: "Enter your answer",
+              },
+            ]}
+            submitHandler={handleFaqSubmit}
+            loading={faqloading}
+            editing={editingItem?.type === "faq"}
+          />
+        </div>
+        {/* Display FAQ */}
+        <div className="p-3">
+          <h1 className="text-xl font-bold mb-4 flex justify-center">FAQ's</h1>
           {faqs.map((faq) => (
-            <div key={faq.id} className="flex justify-between p-2 border">
-              <span>
-                {faq.questions} - {faq.answers}
-              </span>
-              <Button
-                type="submit"
-                variant="outline"
-                onClick={() =>
-                  setEditingItem({ id: faq.id, type: "faq", data: faq })
-                }
-              >
-                Update
-              </Button>
-              <Button
-                type="submit"
-                variant="outline"
-                onClick={() => handleDeleteFaq(faq.id)}
-              >
-                Delete
-              </Button>
+            <div key={faq.id} className="flex flex-col gap-3 justify-between p-4 border">
+              <div className="flex flex-col gap-2 w-[80%]">
+                <span>
+                  {faq.questions}
+                </span>
+                <span>
+                -{faq.answers}
+                </span>
+              </div>
+              <div className="flex gap-3">
+                <Button
+                  type="submit"
+                  variant="outline"
+                  onClick={() =>
+                    setEditingItem({ id: faq.id, type: "faq", data: faq })
+                  }
+                >
+                  Update
+                </Button>
+                <Button
+                  type="submit"
+                  variant="outline"
+                  onClick={() => handleDeleteFaq(faq.id)}
+                >
+                  Delete
+                </Button>
+              </div>
             </div>
           ))}
         </div>
       </div>
       {/* Category */}
-      <div>
-        <h1 className="text-xl font-bold mb-4">
-          {editingItem?.type === "category"
-            ? "Edit Category"
-            : "Create Category"}
-        </h1>
-        <DynamicForm
-          key={editingItem?.id || "new"}
-          schema={categorySchema}
-          defaultValues={
-            editingItem?.type === "category"
-              ? { name: editingItem.data.name }
-              : { name: "" }
-          }
-          fields={[
-            {
-              name: "name",
-              label: "Category",
-              placeholder: "Create a new category",
-            },
-          ]}
-          submitHandler={handleCategorySubmit}
-          loading={categoryLoading}
-          editing={editingItem?.type === "category"}
-        />
-        {categories.map((category) => (
-          <div key={category.id} className="flex justify-between p-2 border">
-            <span>{category.name}</span>
-            <Button
-              type="submit"
-              variant="outline"
-              onClick={() =>
-                setEditingItem({
-                  id: category.id,
-                  type: "category",
-                  data: category,
-                })
-              }
-            >
-              Update
-            </Button>
-            <Button
-              type="submit"
-              variant="outline"
-              onClick={() => handleDeleteCategory(category.id)}
-            >
-              Delete
-            </Button>
-          </div>
-        ))}
+      <div className="my-3">
+        <div className="flex flex-col gap-4 bg-pink-50 items-center p-4">
+          <h1 className="text-xl font-bold mb-4">
+            {editingItem?.type === "category"
+              ? "Edit Category"
+              : "Create Category"}
+          </h1>
+          <DynamicForm
+            key={editingItem?.id || "new"}
+            schema={categorySchema}
+            defaultValues={
+              editingItem?.type === "category"
+                ? { name: editingItem.data.name }
+                : { name: "" }
+            }
+            fields={[
+              {
+                name: "name",
+                label: "Category",
+                placeholder: "Create a new category",
+              },
+            ]}
+            submitHandler={handleCategorySubmit}
+            loading={categoryLoading}
+            editing={editingItem?.type === "category"}
+          />
+        </div>
+        {/* Display Category */}
+        <div className="p-3">
+          {categories.map((category) => (
+            <div key={category.id} className="flex flex-col justify-between p-4 gap-3 border">
+              <span>{category.name}</span>
+              <div className="flex gap-3">
+                <Button
+                  type="submit"
+                  variant="outline"
+                  onClick={() =>
+                    setEditingItem({
+                      id: category.id,
+                      type: "category",
+                      data: category,
+                    })
+                  }
+                >
+                  Update
+                </Button>
+                <Button
+                  type="submit"
+                  variant="outline"
+                  onClick={() => handleDeleteCategory(category.id)}
+                >
+                  Delete
+                </Button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
       {/* DAte */}
-      <div>
-        <h1 className="text-xl font-bold mb-4">
-          {editingItem?.type === "dateEvent"
-            ? "Edit DateEvent"
-            : "Create DateEvent"}
-        </h1>
-        <DynamicForm
-          key={editingItem?.id || "new"}
-          schema={dateSchema}
-          defaultValues={
-            editingItem?.type === "dateEvent"
-              ? {
-                  date: editingItem.data.date
-                    ? new Date(editingItem.data.date)
-                        .toISOString()
-                        .split("T")[0]
-                    : new Date().toISOString().split("T")[0],
-                  eventname: editingItem.data.eventname,
-                  notes: editingItem.data.notes,
-                }
-              : {
-                  date: new Date().toISOString(),
-                  eventname: "",
-                  notes: "",
-                }
-          }
-          fields={[
-            {
-              name: "date",
-              label: "Pick a Date",
-              placeholder: "Select the date",
-              type: "date",
-            },
-            {
-              name: "eventname",
-              label: "Eventname",
-              placeholder: "Enter the eventname",
-            },
-            {
-              name: "notes",
-              label: "Notes",
-              placeholder: "Enter the Summery of the event",
-            },
-          ]}
-          submitHandler={handleDateSubmit}
-          loading={dateLoading}
-          editing={editingItem?.type === "dateEvent"}
-        />
-        {dateEvents.map((event) => (
-          <div key={event.id} className="flex justify-between p-2 border">
-            <span>
-              {new Date(event.date).toLocaleDateString()} - {event.eventname}
-            </span>
-            <Button
-              type="submit"
-              variant={"outline"}
-              onClick={() =>
-                setEditingItem({ id: event.id, type: "dateEvent", data: event })
-              }
-            >
-              Update
-            </Button>
-            <Button
-              type="submit"
-              variant={"outline"}
-              onClick={() => handleDeleteDateEvent(event.id)}
-            >
-              Delete
-            </Button>
-          </div>
-        ))}
+      <div >
+        <div className="flex flex-col gap-4 bg-pink-50 items-center p-4">
+          <h1 className="text-xl font-bold mb-4">
+            {editingItem?.type === "dateEvent"
+              ? "Edit DateEvent"
+              : "Create DateEvent"}
+          </h1>
+          <DynamicForm
+            key={editingItem?.id || "new"}
+            schema={dateSchema}
+            defaultValues={
+              editingItem?.type === "dateEvent"
+                ? {
+                    date: editingItem.data.date
+                      ? new Date(editingItem.data.date)
+                          .toISOString()
+                          .split("T")[0]
+                      : new Date().toISOString().split("T")[0],
+                    eventname: editingItem.data.eventname,
+                    notes: editingItem.data.notes,
+                  }
+                : {
+                    date: new Date().toISOString().split("T")[0],
+                    eventname: "",
+                    notes: "",
+                  }
+            }
+            fields={[
+              {
+                name: "date",
+                label: "Pick a Date",
+                placeholder: "Select the date",
+                type: "date",
+              },
+              {
+                name: "eventname",
+                label: "Eventname",
+                placeholder: "Enter the eventname",
+              },
+              {
+                name: "notes",
+                label: "Notes",
+                placeholder: "Enter the Summery of the event",
+              },
+            ]}
+            submitHandler={handleDateSubmit}
+            loading={dateLoading}
+            editing={editingItem?.type === "dateEvent"}
+          />
+        </div>
+        {/* Display DateEvent */}
+        <div className="p-3">
+          {dateEvents.map((event) => (
+            <div key={event.id} className="flex flex-col justify-between p-4 gap-3 border">
+              <div className="flex flex-col gap-2 w-[80%]">
+                <span>
+                  {new Date(event.date).toLocaleDateString()} - {event.eventname}
+                </span>
+                <span>
+                  {event.notes}
+                </span>
+              </div>
+              <div className="flex gap-3">
+                <Button
+                  type="submit"
+                  variant={"outline"}
+                  onClick={() =>
+                    setEditingItem({ id: event.id, type: "dateEvent", data: event })
+                  }
+                >
+                  Update
+                </Button>
+                <Button
+                  type="submit"
+                  variant={"outline"}
+                  onClick={() => handleDeleteDateEvent(event.id)}
+                >
+                  Delete
+                </Button>
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
+      
+      {/* Customer Reply */}
+      <div className="p-3 my-3">
+        <div className="flex justify-center">
+          <h1 className="text-xl font-bold mb-4">Delete Customer review</h1>
+        </div>
+        <div className="grid grid-cols-3 gap-4 p-3">
+          {customer.length > 0 ? (
+            customer.map((customer) => (
+              <div
+                key={customer.id}
+                className="flex flex-col gap-2 justify-between p-2 border"
+              >
+                <h3>{customer.name}</h3>
+                <span>{customer.email}</span>
+                <p>{customer.comments}</p>
+                <Button
+                  type="submit"
+                  variant="outline"
+                  onClick={() => handleDeleteCustomer(customer.id)}
+                >
+                  Delete
+                </Button>
+              </div>
+            ))
+          ) : (
+            <h3>No customer review found!!</h3>
+          )}
+        </div>
+      </div>
+
+
       {/* Image */}
-      <div>
+      <div className="flex flex-col gap-4 bg-pink-50 items-center p-4">
         <h1 className="text-xl font-bold mb-4">Upload Image</h1>
-        {message && <p className="text-red-500">{message}</p>}
+        {message && <p className="text-pink-500">{message}</p>}
         <form
           onSubmit={handleImageSubmit}
           className="space-y-4 flex flex-col items-start justify-center px-6 pb-5"
@@ -272,54 +340,56 @@ export default function Admin() {
         </form>
       </div>
 
-      {/* Customer Reply */}
-      <div>
-        <h1 className="text-xl font-bold mb-4">Delete Customer review</h1>
-        <div className="grid grid-cols-3 gap-4 p-3">
-          {customer.length > 0 ? (
-            customer.map((customer) => (
-              <div
-                key={customer.id}
-                className="flex flex-col gap-2 justify-between p-2 border"
-              >
-                <h3>{customer.name}</h3>
-                <span>{customer.email}</span>
-                <p>{customer.comments}</p>
-                <Button
-                  type="submit"
-                  variant="outline"
-                  onClick={() => handleDeleteCustomer(customer.id)}
-                >
-                  Delete
-                </Button>
-              </div>
-            ))
-          ) : (
-            <h3>No customer review found!!</h3>
-          )}
-        </div>
+      
+
+      {/* Image Display */}
+      {/* <div className="flex flex-col gap-4"> */}
+      <div className="flex flex-col justify-center items-center">
+        {categories.map((cat) => (
+          <div className="flex flex-col items-center gap-4 my-5" key={cat.id}>
+            <h1 className="text-xl font-bold mb-4">{cat.name}</h1>
+            <Carousel
+              opts={{
+                align: "start",
+              }}
+              className=" max-w-6xl w-[70vw]"
+            >
+              <CarouselContent>
+                {image
+                  .filter((img) => img.catid === cat.id)
+                  .map((img) => (
+                    <CarouselItem
+                      key={img.id}
+                      className="md:basis-1/2 lg:basis-1/3"
+                    >
+                      <div className="flex flex-col items-center aspect-auto min-h-[15em] md:min-h-[16em] justify-center gap-2 p-2 border ">
+                        <Image
+                          src={
+                            img.url.startsWith("http") ? img.url : `/${img.url}`
+                          }
+                          width={900}
+                          height={1200}
+                          alt="uploded"
+                          className="w-auto h-auto object-contain"
+                        />
+                        <Button
+                          type="submit"
+                          variant={"outline"}
+                          onClick={() => handleDeleteImage(img.id)}
+                        >
+                          Delete
+                        </Button>
+                      </div>
+                    </CarouselItem>
+                  ))}
+              </CarouselContent>
+              <CarouselNext />
+              <CarouselPrevious />
+            </Carousel>
+          </div>
+        ))}
       </div>
-      <div className="flex flex-col gap-4">
-        {image
-          .filter((img) => img.catid === 5)
-          .map((img) => (
-            <div key={img.id} className="flex flex-col gap-2 p-2 border">
-              <Image
-                src={img.url.startsWith("http") ? img.url : `/${img.url}`}
-                width={300}
-                height={300}
-                alt="uploded"
-              />
-              <Button
-                type="submit"
-                variant={"outline"}
-                onClick={() => handleDeleteImage(img.id)}
-              >
-                Delete
-              </Button>
-            </div>
-          ))}
-      </div>
+      {/* </div> */}
     </>
   );
 }
